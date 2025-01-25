67fed071cff6d455825fed4d4907118f7c9f7916
Sergio Núñez <sergioop722@gmail.com>
2025-01-24 22:27:15 -0500

    backend - se crea login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,10 +2,12 @@ import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 import { createAccessToken } from '../libs/jwt.js'
 
+
+// REGISTRO DE USUARIO
 export const register = async (req, res) => {
     const { email, password, username } = req.body
 
-    // Crear usuario
+
     try {
 
         // Encriptar password
@@ -38,4 +40,44 @@ export const register = async (req, res) => {
 }
 
 
-export const login = (req, res) => res.send('login')
+// LOGIN
+export const login = async (req, res) => {
+    const { email, password } = req.body
+
+
+    try {
+
+        // Verificar usuario
+        const userFound = await User.findOne({ email });
+        if (!userFound) return res.status(400).json({ message: "Usuario no encontrado" })
+
+        // Verificar password
+        const isMatch = await bcrypt.compare(password, userFound.password)
+        if (!isMatch) return res.status(400).json({ message: 'Contraseña incorrecta' })
+
+        // creamos el token
+        const token = await createAccessToken({ id: userFound._id })
+
+        // Guardar en una cookie
+        res.cookie('token', token)
+        // Respuesta
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createAt: userFound.createdAt,
+            updateAt: userFound.updatedAt
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+
+// Cancelar o Cerrar el TOKEN
+export const logout = (req, res) => {
+    res.cookie('token', "", {
+        expires: new Date(0)
+    })
+    return res.sendStatus(200)
+}
